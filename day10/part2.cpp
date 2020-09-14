4499e5180d7774aa4392dd99992c266fa6960673
Ryan Stephenson <ryanstephenson2013@gmail.com>
2020-09-13 17:20:26 -0700

    day10 p2 solution

--- day10/part2.cpp
+++ day10/part2.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
+#include <algorithm>
 #include <set>
 
 using namespace std;
@@ -36,71 +38,88 @@ vector<string> read_file() {
    // make a new scan for case (X,Y)
 
 int main() {
-    int org_x = 21;
-    int org_y = 22;
+    const int org_x = 20; // astriod of origin
+    const int org_y = 21; // astriod of origin
     int x2,y2; // second position measurment
     float ang; // angle between current and second asteriod 
-    char c,c2; // char at position  
+    char c2; // char at position  
 
     vector<string> data =  read_file(); // load input file
 
 
-    // make a hash table of ast_los[ang] = {astriod @ (x,y)} 
-    // i.e. ast_los= {'0': {(0,1),(0,2),...}, '90': {...}, ..., '359.*': {...} };
-
-    // vector<vector<vector<float>>> ast_los; // asteriod's line of sight: f(x,y)={angles}
-    // vector<vector<float>> w_zeros;
-    // vector<float> e = {}; 
-    // for (int i=0; i<WIDTH;i++) w_zeros.push_back(e);   
-    // for (int j=0;j<HEIGHT;j++) ast_los.push_back(w_zeros);
+    // map of points to corresponding asteriod locations
+    map<float, vector<pair<int,int>>> ast_los ;
 
     cout << "\nStarting..." << endl;
 
 
-            for (int lm=0; lm<WIDTH*HEIGHT;lm++) {
-                x2=lm%WIDTH;
-                y2=lm/HEIGHT;  
-                c2=data[y2][x2];
+    for (int lm=0; lm<WIDTH*HEIGHT;lm++) {
+        x2=lm%WIDTH;
+        y2=lm/HEIGHT;  
+        c2=data[y2][x2];
 
-                ang=0;
-                if (c2 == '#') {    
-                    if (abs(org_y - y2)== 0) {
-                        ang = ((org_x - x2) < 0) ? 90 : 270;
-                    }
-                    else if (abs(org_x-x2) == 0) {
-                        ang = ((org_y - y2) < 0) ? 0 : 180;
-                    } else { 
-                        ang =  (atan2((float)(org_x-x2),(float)(org_y-y2)) * (57.29577951308));
-                        if (ang < 0) {
-                            ang += 360.0;
-                        }
-                    }
-                    // ast_los[y2][x2].insert(ang);
-
-                    
+        ang=0;
+        if (c2 == '#') {    
+            if (abs(org_y - y2)== 0) {
+                ang = ((org_x - x2) > 0) ? 90.0 : 270.0;
+            }
+            else if (abs(org_x-x2) == 0) {
+                ang = ((org_y - y2) > 0) ? 0.0 : 180.0;
+            } else { 
+                ang =  -(atan2((float)(org_x-x2),(float)(org_y-y2)) * (57.29577951308));
+                if (ang < 0) {
+                    ang += 360.0;
                 }
             }
-         
-    int maxV=0;
-    int V=0;
-    int ind_x,ind_y = 1;
-    for(ind_x; ind_x<=WIDTH;ind_x++){
-        printf("%-4d",ind_x);
-    }
-    cout << endl;
-    for (auto it : ast_los) {
-        // cout << ind_y << " ";
-        printf("%-2d",ind_y);
-        ind_y+=1;
-        for (auto ij : it) {
-            V= ij.size();
-            (V == 0) ? printf("%4c",'.') : printf("%4d",V); // why use if statements if you just found out what a ternary is !?
-            //printf("%4d",V);
-            if (V > maxV) 
-                maxV = V;
+            //cout << ang << endl;
+            ast_los[ang].push_back(pair<int,int>(x2,y2));
         }
-        cout << endl;
     }
-    cout << endl << "Max: " << maxV << endl; 
+         
+     int maxV=0;
+     int V=0;
+
+    
+
+   /*
+    * Sorts through the asteriods at each angle relative to the 'origin' astriod  
+    */
+    for (auto angle_points : ast_los) {
+        auto angle = angle_points.first;
+        auto points = angle_points.second;
+        sort(points.begin(),points.end(), 
+                [org_x,org_y](pair<int,int> p1, pair<int,int> p2) -> bool {
+                    bool xchk = (p1.first - org_x) > (p2.first - org_x);
+                    bool ychk = (p1.second- org_y) > (p2.second- org_y);
+                    bool result =  xchk | ychk;
+                    return result;
+                }        
+        );
+            if (1) {
+            //point.pop_back();
+            printf("θ=%0.2f°\n", angle);
+            for (auto p : points) {
+                printf("\t(%d,%d)\n",p.first,p.second);
+            }
+            }
+    }
+
+    int hit =0;
+    pair<int,int> ans;
+    for (auto angle_points : ast_los) {
+        auto angle = angle_points.first;
+        auto points = angle_points.second;
+        if (hit == 200-1){
+            ans = points.back();
+            break;
+        } else{
+        points.pop_back();
+        hit +=1;
+        }
+        
+    }
+
+    printf("200th Asteriod @ \t(%d,%d)\n",ans.first,ans.second); 
+    cout << "ANS:" << ans.first*100 + ans.second << endl;
     cout << "EOF.\n";    
 } // sol: 243 @ (21,22)
